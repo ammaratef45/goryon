701878889931a335912ed2c66474dbb5df7cfb50
Daven Casia <davencasia@gmail.com>
2020-08-21 19:25:50 -0600

    Added images for readme

---
+++ lib/strings.dart
@@ -0,0 +1,9 @@
+class AppStrings {
+  List<String> twtPromtpts = [
+    "What's on your mind?",
+    "Share something insightful!",
+    "Good day to you! What's new?",
+    "Did something cool lately? Share it!",
+    "Hi! 👋 Don't forget to post a Twt today!",
+  ];
+}
