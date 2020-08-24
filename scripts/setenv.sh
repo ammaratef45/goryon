ec0d78dd0ea38b625646063db008cf02c90bdae1
Daven Casia <davencasia@gmail.com>
2020-08-23 22:50:36 -0600

    Simplified shell script

--- scripts/setenv.sh
+++ scripts/setenv.sh
@@ -1,9 +1,5 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-echo "Script absolute path is $SCRIPT_DIR"
-PROJ_ROOT="$(dirname "$SCRIPT_DIR")"
-echo "Project root path is $PROJ_ROOT"
 
 if [ -z "${ANDROID_KEY_STORE_PWD}" ]; then
   echo "ANDROID_KEY_STORE_PWD not found, abort"
@@ -19,19 +15,18 @@ else
   echo "ANDROID_KEY_PWD detected, proceeding..."
 fi
 
-# Create the keystore for signing the Android app.
-rm -f "${PROJ_ROOT}"/android/key.properties
 {
   echo "storePassword=${ANDROID_KEY_STORE_PWD}"
   echo "keyPassword=${ANDROID_KEY_PWD}"
   echo "keyAlias=key"
   echo "storeFile=key.jks"
-} >>"$PROJ_ROOT"/android/key.properties
+} >> android/key.properties
 
 echo "${RELEASE_KEYSTORE}" > release.keystore.asc
-gpg -d --passphrase "${RELEASE_KEYSTORE_PASSPHRASE}" --batch release.keystore.asc >"${PROJ_ROOT}"/android/app/key.jks
+gpg -d --passphrase "${RELEASE_KEYSTORE_PASSPHRASE}" --batch release.keystore.asc > android/app/key.jks
 
 
-mkdir -p "$PROJ_ROOT"/keys
+mkdir -p keys/android
 echo "${RELEASE_SERVICE_ACCOUNT_KEYSTORE}" > service_account.keystore.asc
-gpg -d --passphrase "${RELEASE_KEYSTORE_PASSPHRASE}" --batch service_account.keystore.asc >"${PROJ_ROOT}"/keys/android/service_account.json
+gpg -d --passphrase "${RELEASE_KEYSTORE_PASSPHRASE}" --batch service_account.keystore.asc > keys/android/service_account.json
+
